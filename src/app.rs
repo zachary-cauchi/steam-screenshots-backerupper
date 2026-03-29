72dc554b639ad3e625e26d2bc68051e420de1a6a
Zachary Cauchi <zach.cauchi@hotmail.com>
2026-03-29 18:04:12 +0200

    feat: Find proper steam install dir

--- src/app.rs
+++ src/app.rs
@@ -4,6 +4,8 @@ use tracing::{debug, error, info, warn};
 
 use crate::result::{CrateError, CrateResult};
 
+const HOME_STEAM_DIR: &str = ".steam/steam";
+
 pub struct App {}
 
 impl App {
@@ -21,16 +23,39 @@ impl App {
     }
 
     /// Find the Steam installation dir.
+    /// This is done by looking for "~/.steam/steam" and following the sym-link to the actual install dir.
     fn find_steam_dir(&self) -> CrateResult<PathBuf> {
         info!("Getting Steam directory.");
 
         let Some(home_dir) = std::env::home_dir() else {
             warn!("Failed to find user home dir.");
-            return Err(CrateError::DirNotFound("User home dir."));
+            return Err(CrateError::dir_not_found("User home dir."));
         };
 
         debug!("Found user home dir ({}).", home_dir.display());
 
-        Ok(home_dir)
+        debug!("Testing for `.steam/steam` dir existance.");
+
+        let home_steam_dir = home_dir.join(HOME_STEAM_DIR);
+
+        if !(home_steam_dir.exists() && home_steam_dir.is_dir()) {
+            warn!(
+                "Could not find user's home steam dir ({}).",
+                home_steam_dir.display()
+            );
+
+            return Err(CrateError::dir_not_found(home_steam_dir.to_string_lossy()));
+        }
+
+        debug!("Found user home steam dir ({}).", home_steam_dir.display());
+
+        let steam_dir = home_steam_dir.canonicalize().inspect_err(|e| {
+            warn!(
+                "Failed to resolve home steam dir ({}). Error: {e}",
+                home_steam_dir.display()
+            )
+        })?;
+
+        Ok(steam_dir)
     }
 }
--- src/result.rs
+++ src/result.rs
@@ -4,16 +4,21 @@ use std::{error::Error, fmt::Display};
 #[derive(Debug)]
 pub enum CrateError {
     /// The expected directory was not found.
-    DirNotFound(&'static str),
+    DirNotFound(String),
+    IoError(std::io::Error),
 }
 
-/// The crate-standard [Result] type.
-pub type CrateResult<T> = Result<T, CrateError>;
+impl CrateError {
+    pub fn dir_not_found(dir: impl ToString) -> Self {
+        Self::DirNotFound(dir.to_string())
+    }
+}
 
 impl Display for CrateError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::DirNotFound(dir) => write!(f, "Directory not found (\"{dir}\")"),
+            Self::IoError(e) => write!(f, "General IO Error ({e})"),
         }
     }
 }
@@ -22,6 +27,16 @@ impl Error for CrateError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
             Self::DirNotFound(_) => None,
+            Self::IoError(e) => Some(e),
         }
     }
 }
+
+impl From<std::io::Error> for CrateError {
+    fn from(value: std::io::Error) -> Self {
+        Self::IoError(value)
+    }
+}
+
+/// The crate-standard [Result] type.
+pub type CrateResult<T> = Result<T, CrateError>;
