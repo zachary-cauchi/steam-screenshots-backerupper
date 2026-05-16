4f582c9b4a95e999ba93769e17625735782f90cd
Zachary Cauchi <zach.cauchi@hotmail.com>
2026-05-17 00:27:55 +0200

    feat: Add getting of game name via steam store

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,14 @@
-use tracing::{error, info};
+use serde_json::Value;
+use tracing::{error, info, warn};
+use ureq::http::{uri::Scheme, StatusCode, Uri};
 
-use crate::{result::CrateResult, steam::Steam};
+use crate::{
+    result::{CrateError, CrateResult},
+    steam::Steam,
+};
+
+const JSNLI_GAME_APPID_LIST_URL: &str =
+    "https://github.com/jsnli/steamappidlist/blob/master/data/games_appid.json";
 
 pub struct App {}
 
@@ -19,9 +27,56 @@ impl App {
         let screenshots_dirs = steam_dir.get_game_screenshot_dirs()?;
 
         for screenshots_dir in screenshots_dirs {
-            info!("Game dir: {:?}", screenshots_dir)
+            info!("Game dir: {:?}", screenshots_dir);
+
+            let Some(game_id) = screenshots_dir.file_name() else {
+                warn!("Path has no name. Path: {}", screenshots_dir.display());
+                continue;
+            };
+            let game_id = game_id.to_string_lossy();
+
+            info!("Fetching name of game '{}'.", game_id);
+
+            let Some(name) = self.get_name_from_steamstore(game_id.as_ref())? else {
+                warn!("No name for Game ID '{game_id}' found.");
+                continue;
+            };
+
+            info!("Game name: {name}");
         }
 
         Ok(())
+    }
+
+    /// Gets the game name using the Steam webstore API.
+    /// There may be a
+    fn get_name_from_steamstore(&self, game_id: &str) -> CrateResult<Option<String>> {
+        const STEAM_STORE_API_URL: &str = "https://store.steampowered.com/api/appdetails";
+
+        let url = format!("{STEAM_STORE_API_URL}?appids={}", game_id.to_string());
+        info!("Calling '{url}'.");
+
+        let mut response = ureq::get(url).call()?;
+
+        if response.status() != StatusCode::OK {
+            warn!(
+                "Non-OK status. API call returned status '{}'",
+                response.status()
+            );
+        }
+
+        let game_json: Value = response.body_mut().read_json()?;
+
+        if game_json[game_id]["success"] != Value::Bool(true) {
+            return Ok(None);
+        }
+
+        let name = match &game_json[game_id]["data"]["name"] {
+            Value::String(name) => name.clone(),
+            Value::Null => return Err(CrateError::DataNotFound(format!("{game_id}.data.name"))),
+            v => return Err(CrateError::WrongType(("String", "Non-string type"))),
+        };
+
+        Ok(Some(name))
     }
 }
--- src/result.rs
+++ src/result.rs
@@ -5,14 +5,22 @@ use std::{error::Error, fmt::Display, num::ParseIntError};
 pub enum CrateError {
     /// The expected directory was not found.
     DirNotFound(String),
+    DataNotFound(String),
+    WrongType((&'static str, &'static str)),
+    GeneralError(String),
     FilePathing(&'static str),
     IntParsing(ParseIntError),
     IoError(std::io::Error),
+    NetworkError(ureq::Error),
 }
 
 impl CrateError {
     pub fn dir_not_found(dir: impl ToString) -> Self {
         Self::DirNotFound(dir.to_string())
+    }
+
+    pub fn wrong_type(expected: &'static str, actual: &'static str) -> Self {
+        Self::WrongType((expected, actual))
     }
 }
 
@@ -20,9 +28,13 @@ impl Display for CrateError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::DirNotFound(dir) => write!(f, "Directory not found (\"{dir}\")"),
+            Self::DataNotFound(data) => write!(f, "Missing data '{data}'"),
             Self::FilePathing(msg) => write!(f, "File path parsing error (\"{msg}\")"),
+            Self::GeneralError(msg) => write!(f, "General error: '{msg}'"),
             Self::IntParsing(e) => write!(f, "Integer parsing error (\"{e}\")"),
             Self::IoError(e) => write!(f, "General IO Error ({e})"),
+            Self::NetworkError(e) => write!(f, "Network/HTTP error ({e})"),
+            Self::WrongType((e, a)) => write!(f, "Wrong data type. Expected: {e}, Actual: {a}"),
         }
     }
 }
@@ -30,9 +42,10 @@ impl Display for CrateError {
 impl Error for CrateError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            Self::DirNotFound(_) | Self::FilePathing(_) => None,
             Self::IntParsing(e) => Some(e),
             Self::IoError(e) => Some(e),
+            Self::NetworkError(e) => Some(e),
+            _ => None,
         }
     }
 }
@@ -46,6 +59,12 @@ impl From<std::io::Error> for CrateError {
 impl From<std::num::ParseIntError> for CrateError {
     fn from(value: std::num::ParseIntError) -> Self {
         Self::IntParsing(value)
+    }
+}
+
+impl From<ureq::Error> for CrateError {
+    fn from(value: ureq::Error) -> Self {
+        Self::NetworkError(value)
     }
 }
 
